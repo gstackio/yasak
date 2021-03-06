8a19dd7e96259c363ebe2e084409974e2b8ec089
Benjamin Gandon <benjamin@gstack.io>
2021-03-06 20:05:19 +0100

    Use PATH for yasak lookup + Fix 'yasak locate' invocation + Fix sed expression for Linux

--- example-edit-value.sh
+++ example-edit-value.sh
@@ -6,17 +6,18 @@ function main() {
     local new_value=$3
 
     local coords line column line_comment
-    coords=$(./yasak locate example.yml --path "${path}")
+    coords=$(yasak locate "${file}" --path "${path}")
     line=$(cut -f 1 <<< "${coords}")
     column=$(cut -f 2 <<< "${coords}")
     line_comment=$(cut -f 3- <<< "${coords}")
 
-    local replacement
+    local replacement repeat
     replacement="${new_value}"
     if [[ -n ${line_comment} ]]; then
         replacement+=" ${line_comment}"
     fi
-    sed -e "${line}s/^\\(.\\{$((${column} - 1))\\}\\).*$/\\1${replacement}/" "${file}"
+    repeat=$((${column} - 1))
+    sed -e "${line}s/^\\(.\\{${repeat},${repeat}\\}\\).*$/\\1${replacement}/" "${file}"
 }
 
 main "$@"
