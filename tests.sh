48f3b9c064a13921f0f8dc3f5f8b455791228fba
Bertrand Lupart <bertrand@gstack.io>
2021-04-28 13:56:28 +0200

    Fix tests

--- tests.sh
+++ tests.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-./yasak locate example.yml --path /bim/bam/toto
+./yasak locate "example.yml" --path "/bim/bam/toto"
 
-./yasak locate example.yml --path /bim/bam/toti
+./yasak locate "example.yml" --path "/bim/bam/toti"
 
-./yasak locate example.yml --path /bim/bam/toti?
+./yasak locate "example.yml" --path "/bim/bam/toti?"
 
-./yasak locate example.yml --path /bim/boum/toto
+./yasak locate "example.yml" --path "/bim/boum/toto"
 
-./yasak locate example.yml --path /bim/boum?/toto
+./yasak locate "example.yml" --path "/bim/boum?/toto"
 
-bash example-edit-value.sh /bim/bam/toto plop
+bash example-edit-value.sh "example.yml" "/bim/bam/toto" "plop"
